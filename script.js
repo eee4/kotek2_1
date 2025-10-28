a88227373b8c2147bd868b95b21f06a9ed83864b
eee4 <41441600+eee4@users.noreply.github.com>
2025-10-28 11:14:58 +0100

    Com2_1.1

--- script.js
+++ script.js
@@ -9,13 +9,35 @@
     fetch('https://jsonplaceholder.typicode.com/posts')
       .then(response => response.json())
       .then(array => {
-        console.log(array)
+        console.log(array);
         answer.innerHTML = JSON.stringify(array);
       })
   })
 
+  // Zad. 2_1.1.
   cw1.addEventListener("click", function () {
-    //TODO
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(array => {
+        console.log(array);
+        answer.innerHTML = "";
+        for (const o of array) {
+          let div        = document.createElement("div");
+          let postMeta   = document.createElement("p");
+          let postTitle  = document.createElement("h3");
+          let postBody   = document.createElement("p");
+
+          div.classList.add("generic_post");
+          div.id = `post${o.id}`;
+
+          postMeta.append(`Post #${o.id} by user #${o.userId}`);
+          postTitle.append(o.title);
+          postBody.insertAdjacentHTML("beforeend", `<i>${o.body.replace("\n", "<br/>")}</i>`);
+          div.append(postMeta, postTitle, postBody);
+          
+          answer.append(div);
+        }
+      })
   })
 
   cw2.addEventListener("click", function () {
