4bd3b6fd13c2233f5ea2bcfc28a4bf4beebad456
eee4 <41441600+eee4@users.noreply.github.com>
2025-10-28 11:59:29 +0100

    Com2_1.4

--- script.js
+++ script.js
@@ -4,8 +4,13 @@
   const cw2 = document.getElementById('cw2')
   const cw3 = document.getElementById('cw3')
   const answer = document.getElementById('answer')
+  const create = document.getElementById('create');
+  const form = document.querySelector("#postData");
 
   example.addEventListener("click", function () {
+
+    create.hidden = true;
+
     fetch('https://jsonplaceholder.typicode.com/posts')
       .then(response => response.json())
       .then(array => {
@@ -16,6 +21,8 @@
 
   // Zad. 2_1.1., 2_1.2.
   cw1.addEventListener("click", function () {
+
+    create.hidden = true;
 
     const loadingText = document.createElement("div");
     loadingText.insertAdjacentHTML("beforeend", "<span style=\"color: blue;\"><b>Loading...</b></span>");
@@ -50,6 +57,9 @@
 
   // Zad. 2_1.3.
   cw2.addEventListener("click", function () {
+
+    create.hidden = true;
+
     const loadingText = document.createElement("div");
     loadingText.insertAdjacentHTML("beforeend", "<span style=\"color: blue;\"><b>Loading...</b></span>");
     answer.append(loadingText);
@@ -78,8 +88,48 @@
     }, 2000);
   })
 
+  // Zad. 2_1.4.
+
+  function sendData() {
+    // Associate the FormData object with the form element
+    const formData = new FormData(form);
+    var formDataObject = {};
+    formData.forEach(function(value, key){
+      formDataObject[key] = value;
+    });
+    var formDataAsJson = JSON.stringify(formDataObject);
+
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+      method: 'POST',
+      body: formDataAsJson,
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    })
+      .then((response) => response.json())
+      .then((json) => {
+        console.log(json);
+
+        const resultText = document.createElement("div");
+        resultText.insertAdjacentHTML("beforeend", `<br/><span style=\"color: green;\"><b>Dodano nowy post o ID = ${json.id}</b></span>`);
+        answer.append(resultText);
+
+        setTimeout(() => {
+          if (!create.hidden) answer.innerHTML = "";
+        }, 5000);
+
+      });
+
+  }
+
+  form.addEventListener("submit", (ev) => {
+    ev.preventDefault();
+    sendData();
+  });
+
   cw3.addEventListener("click", function () {
-    //TODO
+    create.hidden = false;
+    answer.innerHTML = "";
   })
 
 })();
